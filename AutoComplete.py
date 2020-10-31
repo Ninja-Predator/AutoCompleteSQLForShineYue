efee3c0c6caab6f82a15e0410645a8858a7641ae
栗子拟态怪 <772340161@qq.com>
2020-10-31 11:46:04 +0800

    添加从剪贴板拿取，优化获取逻辑

--- AutoComplete.py
+++ AutoComplete.py
@@ -1,5 +1,4 @@
 # -*- coding: UTF-8 -*-
-from pathlib import Path
 
 import win32clipboard
 import win32con
@@ -7,21 +6,23 @@ import win32con
 
 def format_sql():
     try:
-        my_file = Path("sql.txt")
         p = "Preparing: "
         ps = "Parameters: "
         finalSQL = ""
-        state = "啥都没有"
-        if not my_file.exists():
-            clipboard = get_clipboard()
-            lines = clipboard.split('\n')
-            for line in lines:
-                if p in line:
-                    PSQL = line.split(p)[-1].split('?')
-                elif ps in line:
-                    Parameters = line.split(ps)[-1].split('), ')
-            state = "从剪贴板取"
-        else:
+
+        clipboard = get_clipboard()
+        lines = clipboard.split('\n')
+        for line in lines:
+            if p in line:
+                PSQL = line.split(p)[-1].split('?')
+            elif ps in line:
+                Parameters = line.split(ps)[-1].split('), ')
+        state = "从剪贴板取"
+        complete_sql(PSQL, Parameters, finalSQL, state)
+
+    except UnboundLocalError:
+        print("剪贴板中没有发现东西哦")
+        try:
             f = open("sql.txt", "r", encoding="utf-8")
             for line in f:
                 if p in line:
@@ -30,30 +31,33 @@ def format_sql():
                     Parameters = line.split(ps)[-1].split('), ')
             f.close()
             state = "从文件中取"
+            complete_sql(PSQL, Parameters, finalSQL, state)
+        except FileNotFoundError:
+            print("文件也妹找到。需要用文件功能的话，将需要拼接的语句放在同目录下的sql.txt中")
 
-        if len(PSQL) != len(Parameters) + 1:
-            print("拆分长度出错")
-            print("SQL长度: ", len(PSQL))
-            print("参数长度: ", len(Parameters))
-            print("取值状态为: ", state)
-        else:
-            finalSQL += PSQL[0]
-            for index, ParameterW in enumerate(Parameters):
-                Parameter = ParameterW[:ParameterW.rfind('(')]
-                St = ParameterW.split('(')[-1]
-                if 'String' in St:
-                    Parameter = '\'' + Parameter + '\''
-                finalSQL += Parameter
-                finalSQL += PSQL[index + 1]
-            finalSQL.rstrip('\n')
-            finalSQL += ';'
-            print(finalSQL)
-            print(state)
-            set_clipboard(finalSQL)
-    except FileNotFoundError:
-        print("将需要拼接的语句放在同目录下的sql.txt中")
-    except UnboundLocalError:
-        print("请检查剪贴板的sql语句格式是否正确")
+
+def complete_sql(psql, parameters, final_sql, state):
+    if len(psql) != len(parameters) + 1:
+        print("拆分长度出错")
+        print("SQL长度: ", len(psql))
+        print("参数长度: ", len(parameters))
+        print("是", state, "的哦")
+    else:
+        final_sql += psql[0]
+        for index, ParameterW in enumerate(parameters):
+            Parameter = ParameterW[:ParameterW.rfind('(')]
+            St = ParameterW.split('(')[-1]
+            if 'String' in St:
+                Parameter = '\'' + Parameter + '\''
+            final_sql += Parameter
+            final_sql += psql[index + 1]
+        final_sql = final_sql.rstrip('\r')
+        final_sql = final_sql.rstrip('\n')
+        final_sql = final_sql.rstrip(' ')
+        final_sql += ';'
+        print(final_sql)
+        print("是", state, "的哦")
+        set_clipboard(final_sql)
 
 
 def get_clipboard():
--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 # SQL语句自动补齐
 对神玥mybatis框架控制台输出的SQL语句进行自动补全 <br>
-将控制台复制出来的带有?占位符的SQL语句和参数复制到[sql.txt](sql.txt)中，执行AutoComplete.py，即可获得补齐后的sql语句
+将控制台复制出来的带有?占位符的SQL语句和参数复制到[sql.txt](sql.txt)中
+，执行AutoComplete.py，即可获得补齐后的sql语句<br>
+当然如果你懒得粘了，它也可以从你的剪贴板里读。<br>
+最后生成的补全后的sql语句会放在你的剪贴板里。
